5b9e2004e184c8fdfd364b3fdcd9049459d02591
erickar24 <146250991+erickar24@users.noreply.github.com>
2023-11-18 16:10:00 -0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([37.8, -96], 4);
-
-var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/roseeric/clonhuvxj003p01r6adif9nxw.html?title=copy&access_token=pk.eyJ1Ijoicm9zZWVyaWMiLCJhIjoiY2xvaXlvd204MWg0NDJscWV1Z213bngwMiJ9.SfIEUGSweh97VUrrniVDJQ&zoomwheel=true&fresh=true#7.43/43.888/-123.487', {
-    maxZoom: 17,
-    }).addTo(map);
- 
